9c631ab369195bf11d712bc671fd7d3df96302e0
Rakesh Viyak <rakeshviyak@gmail.com>
2014-02-03 00:51:27 +0800

    change the path to load the files

--- js/project.js
+++ js/project.js
@@ -14,8 +14,8 @@ app.value('fbURL', 'https://blinding-fire-2115.firebaseio.com/');
 //Routing Configuration
 app.config(function($routeProvider){
   $routeProvider
-    .when('/', {templateUrl: '/AngularJS Test/views/products.html', controller: 'ProductController'})
-    .when('/new', {templateUrl: '/AngularJS Test/views/upload.html', controller: 'CreateController' })
-    .when('/edit/:productId', {templateUrl: '/AngularJS Test/views/upload.html', controller: 'EditController' })
+    .when('/', {templateUrl: '/views/products.html', controller: 'ProductController'})
+    .when('/new', {templateUrl: '/views/upload.html', controller: 'CreateController' })
+    .when('/edit/:productId', {templateUrl: '/views/upload.html', controller: 'EditController' })
     .otherwise({ redirectTo:'/'});
 });
